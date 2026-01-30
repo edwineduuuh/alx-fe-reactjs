927023b13a0c034553287a6f6229167a28608e3a
syntax-elpadrino <eduardoeduuh@proton.me>
2026-01-30 21:32:09 +0300

    Inline styling in react

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,21 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import UserProfile from "./components/UserProfile";
+import WelcomeMessage from "./components/WelcomeMessage";
+
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <WelcomeMessage />
+    </div>
+    
+  )
+}
