a539931bf4ae7cead146b4358a95bedd887b67dd
syntax-elpadrino <eduardoeduuh@proton.me>
2026-01-30 20:36:39 +0300

    user profile card using props

--- alx-react-app/src/components/userProfile.jsx
+++ alx-react-app/src/components/userProfile.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 export default function UserProfile(props) {
   return (
     <div>
-          <h2>{props.name}</h2>
-          <p>Age: {props.age}</p>
+      <h2>{props.name}</h2>
+      
+      <p>Age: {props.age}</p>
+      
           <p>Bio: {props.bio}</p>
     </div>
   )
