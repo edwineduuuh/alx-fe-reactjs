5b094f7099992d6d7ea50028f1a4db157221190a
syntax-elpadrino <eduardoeduuh@proton.me>
2026-01-30 05:58:15 +0300

    create specific components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,18 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
 import WelcomeMessage from "./components/WelcomeMessage";
 
 import React from 'react'
 
 export default function App() {
   return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer/>
       <WelcomeMessage />
+    </div>
+    
   )
 }
