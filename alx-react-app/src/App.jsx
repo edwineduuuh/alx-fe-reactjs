0ed8ca93c95b1c8875ab8df7b006fe5e2a8b1ec8
syntax-elpadrino <eduardoeduuh@proton.me>
2026-01-30 20:40:47 +0300

    user profile card using props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
-import UserProfile from "./components/userProfile";
+import UserProfile from "./components/UserProfile";
 import WelcomeMessage from "./components/WelcomeMessage";
 
 
