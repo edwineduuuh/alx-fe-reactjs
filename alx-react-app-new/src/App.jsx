8401e37a1efa294110a606ea87f433565c3f10cd
syntax-elpadrino <eduardoeduuh@proton.me>
2026-01-31 13:33:43 +0300

    State and hooks

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,3 +1,4 @@
+import Counter from "./components/Counter";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
@@ -15,6 +16,7 @@ export default function App() {
       <Footer />
        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <WelcomeMessage />
+      <Counter />
     </div>
     
   )
