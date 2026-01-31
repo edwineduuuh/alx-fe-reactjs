3dc15e5ed163dcdb1462e344c67b2f4d6f96fc15
syntax-elpadrino <eduardoeduuh@proton.me>
2026-01-31 15:53:04 +0300

    usercontext

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -4,11 +4,12 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import UserProfile from "./components/UserProfile";
 import WelcomeMessage from "./components/WelcomeMessage";
-
-
 import React from 'react'
+import UserContext from "./UserContext";
+import ProfilePage from "../../alx-react-app-props/src/components/ProfilePage";
 
 export default function App() {
+  const userData = {name:"John Doe", email: "johndoe@gmail.com"}
   return (
     <div>
       <Header />
@@ -17,6 +18,9 @@ export default function App() {
        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <WelcomeMessage />
       <Counter />
+      <UserContext.Provider value={userData}>
+          <ProfilePage />
+      </UserContext.Provider>
     </div>
     
   )
