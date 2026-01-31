0dfd40d7c8de4809a0af12231a4a864994d2c93c
syntax-elpadrino <eduardoeduuh@proton.me>
2026-01-31 15:57:25 +0300

    Usercontext

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -4,7 +4,6 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import UserProfile from "./components/UserProfile";
 import WelcomeMessage from "./components/WelcomeMessage";
-import React from 'react'
 import UserContext from "./UserContext";
 import ProfilePage from "../../alx-react-app-props/src/components/ProfilePage";
 
